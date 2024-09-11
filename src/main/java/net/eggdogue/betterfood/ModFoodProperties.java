053eb532967b450c009c3bbf7de50a6b0b019489
Egg <106157242+EggDogue@users.noreply.github.com>
2024-09-11 19:21:37 +0200

    Cured Rotten Flesh effect added

--- src/main/java/net/eggdogue/betterfood/ModFoodProperties.java
+++ src/main/java/net/eggdogue/betterfood/ModFoodProperties.java
@@ -1,13 +1,17 @@
 package net.eggdogue.betterfood;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
-
 public class ModFoodProperties {
+    private static MobEffectInstance getAbsorptionEffect() {
+        return new MobEffectInstance(MobEffects.ABSORPTION, 100, 0);
+    }
     public static final FoodProperties COOKED_EGG = new FoodProperties.Builder().nutrition(4).saturationModifier(0.25f).build();
     public static final FoodProperties APPLE_PIE = new FoodProperties.Builder().nutrition(8).saturationModifier(0.75f).build();
     public static final FoodProperties BERRY_PIE = new FoodProperties.Builder().nutrition(7).saturationModifier(0.75f).build();
     public static final FoodProperties BAKED_BEETROOT = new FoodProperties.Builder().nutrition(4).saturationModifier(0.50f).build();
     public static final FoodProperties ROASTED_PUMPKIN_SEEDS = new FoodProperties.Builder().nutrition(2).saturationModifier(0.10f).build();
-    public static final FoodProperties CURED_ROTTEN_FLESH = new FoodProperties.Builder().nutrition(5).saturationModifier(0.50f).build();
+    public static final FoodProperties CURED_ROTTEN_FLESH = new FoodProperties.Builder().nutrition(5).saturationModifier(0.50f).effect(getAbsorptionEffect(), 1.0f).build();
 }
